51ae00c78d9a194e6e45c4cd971997433380e354
IDDQDonly <96200101+IDDQDonly@users.noreply.github.com>
2021-12-17 10:43:57 +0400

    Add files via upload

---
+++ java_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+new_version=$(dnf search openjdk > /dev/null 2>&1 | grep "openjdk.x86_64"  2>&1 | sed -n 2p | awk -F "-" {print'$2'})
+
+java_version=$(java -version > /dev/null 2>&1 | grep "version" | awk '{print substr($3,2,2)}')
+
+java_update=$(dnf search openjdk | grep "openjdk.x86_64" 2>&1 | sed -n 2p | awk -F ":" {print'$1'})
+
+if [ $new_version == $java_version ]; then
+	echo "No updates required"
+else
+	dnf install $java_update
+fi
